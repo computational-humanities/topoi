bda044cc6185ac11fa7846b162da829d7cd8aff1
Malte Vogl <mvogl@mpiwg-berlin.mpg.de>
2016-08-12 16:54:47 +0200

    First doc

---
+++ searchindex.js
@@ -0,0 +1,1 @@
+Search.setIndex({docnames:["index"],envversion:50,filenames:["index.rst"],objects:{},objnames:{},objtypes:{},terms:{content:0,index:0,modul:0,page:0,search:0},titles:["Welcome to Computational Humanities &#8211; Topoi&#8217;s documentation!"],titleterms:{comput:0,document:0,human:0,indice:0,tabl:0,topoi:0,welcom:0}})

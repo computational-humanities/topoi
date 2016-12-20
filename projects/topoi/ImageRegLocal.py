9d44bb9a97f4785420146f22b9e0f1ae7f47ab7b
grobian <gordon-fischer@gmx.de>
2016-12-20 10:02:21 +0100

    Update ImageRegLocal.py

--- projects/topoi/ImageRegLocal.py
+++ projects/topoi/ImageRegLocal.py
@@ -11,13 +11,11 @@ def ImageRegistration(imagelist, reference):
     :returns: only data export
     
     :example:
-        >>> Aphrodisias: [http://141.20.159.91/notebooks/constructionaldrawings/aphrodisias/AnalysisImagesRegistration.ipynb]
+        >>> 
         
     """
       for x in range(0, len(imagelist)):
-            #sub.Popen(['scp', imagelist[x], server, ':', path])
-            
-            print('execute script: math -script run_imagereg_dynamic.m '+reference[0]+' '+imagelist[x]+' '+project)
-            script = 'math -script run_imagereg_dynamic.m  '+reference[0]+' '+imagelist[x]+' '+project
+            print('execute script: math -script run_registration.m '+reference[0]+' '+imagelist[x])
+            script = 'math -script run_registration.m  '+reference[0]+' '+imagelist[x]
             p = sub.Popen(['ssh', 'mathematica@141.20.159.97',script],stdout=sub.PIPE,stderr=sub.PIPE) 
     return;

72275eb207a2b52e1c9c18cfc114adfad61b7195
grobian <gordon-fischer@gmx.de>
2016-12-20 09:59:54 +0100

    Update run_registration.m

--- projects/topoi/run_registration.m
+++ projects/topoi/run_registration.m
@@ -2,8 +2,8 @@
 (* ::Input:: *)
 fileName1=ToString[$CommandLine[[4]]];
 fileName2=ToString[$CommandLine[[5]]];
-imagename1=StringSplit[fileName1,'.'][[0]];
-imagename2=StringSplit[fileName2, '.'][[0]];
+imagename1=StringSplit[fileName1,"."][[1]];
+imagename2=StringSplit[fileName2,"."][[1]];
 Needs["GeneralUtilities`"]
 (* ::Input:: *)
 ({i1,i2}={Import[fileName1],Import[fileName2]};
@@ -11,6 +11,5 @@ y12=ImageCorrespondingPoints[i1,i2];
 z12=FindGeometricTransform[y12[[1]], y12[[2]]];
 If[Length[y12[[1]]]>1 ,
    {filetemp=y12;
-    Export["TransformMatrix"<>fileName1<>"_"<>fileName2<>".csv",z12[[2,1]],"CSV"]},""];)
+    Export["TransformMatrix"<>imagename1<>"_"<>imagename2<>".csv",z12[[2,1]],"CSV"]},""];)
 (* ::Input:: *)
-
